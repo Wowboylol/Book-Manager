0e973459787650b65608028b0dff169f471d8da9
Lionel Song <99521745+Wowboylol@users.noreply.github.com>
2022-11-24 22:23:38 -0800

    Component setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,30 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { TagsComponent } from './tags/tags.component';
+import { BooksComponent } from './books/books.component';
+import { BookListComponent } from './books/book-list/book-list.component';
+import { BookDetailComponent } from './books/book-detail/book-detail.component';
+import { BookItemComponent } from './books/book-list/book-item/book-item.component';
+import { TagEditComponent } from './tags/tag-edit/tag-edit.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		HeaderComponent,
+		TagsComponent,
+		BooksComponent,
+		BookListComponent,
+		BookDetailComponent,
+		BookItemComponent,
+		TagEditComponent
+	],
+	imports: [
+		BrowserModule
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
+
 export class AppModule { }
