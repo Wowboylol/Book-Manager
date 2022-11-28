63ef58b8d727b9bfeaa468a55db80cfad460bb87
Lionel Song <99521745+Wowboylol@users.noreply.github.com>
2022-11-28 14:05:24 -0800

    Added book model

---
+++ src/app/books/book.model.ts
@@ -0,0 +1,15 @@
+export class Book
+{
+    public name:string;
+    public description:string;
+    public imagePath:string;
+    public rating:number;
+
+    public constructor(name:string, description:string, imagePath:string, rating:number)
+    {
+        this.name = name;
+        this.description = description;
+        this.imagePath = imagePath;
+        this.rating = rating;
+    }
+}
