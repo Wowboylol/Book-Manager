8b93c58b375fbbb9de5dd475fede28956d36a39e
Lionel Song <99521745+Wowboylol@users.noreply.github.com>
2022-11-29 02:30:32 -0800

    Added link attribute to book model

--- src/app/books/book-list/book-list.component.ts
+++ src/app/books/book-list/book-list.component.ts
@@ -15,9 +15,9 @@ export class BookListComponent implements OnInit
 	public constructor() 
 	{ 
 		this._books = [
-			new Book("Book1", "Placeholder", "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1543945452i/43125355.jpg", 4),
-			new Book("Book2", "Placeholder", "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1543945452i/43125355.jpg", 5),
-			new Book("Book3", "Placeholder", "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1543945452i/43125355.jpg", 3)
+			new Book("Book1", "Placeholder description 1", "Placeholder link 1", "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1543945452i/43125355.jpg", 4),
+			new Book("Book2", "Placeholder description 2", "Placeholder link 2", "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1543945452i/43125355.jpg", 5),
+			new Book("Book3", "Placeholder description 3", "Placeholder link 3", "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1543945452i/43125355.jpg", 3)
 		];
 	}
 
--- src/app/books/book.model.ts
+++ src/app/books/book.model.ts
@@ -2,13 +2,15 @@ export class Book
 {
     public name:string;
     public description:string;
+    public link:string;
     public imagePath:string;
     public rating:number;
 
-    public constructor(name:string, description:string, imagePath:string, rating:number)
+    public constructor(name:string, description:string, link:string, imagePath:string, rating:number)
     {
         this.name = name;
         this.description = description;
+        this.link = link;
         this.imagePath = imagePath;
         this.rating = rating;
     }
