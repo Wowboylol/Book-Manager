d5e7a89335768b52ec4946731d4dc4d40195e2ff
Lionel Song <99521745+Wowboylol@users.noreply.github.com>
2022-11-29 23:31:58 -0800

    Updated book formatting

--- src/app/books/book.service.ts
+++ src/app/books/book.service.ts
@@ -9,9 +9,24 @@ export class BookService
     public constructor() 
 	{ 
 		this.books = [
-			new Book("Book1", "Placeholder description 1", "Placeholder link 1", "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1543945452i/43125355.jpg", 4, []),
-			new Book("Book2", "Placeholder description 2", "Placeholder link 2", "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1543945452i/43125355.jpg", 5, []),
-			new Book("Book3", "Placeholder description 3", "Placeholder link 3", "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1543945452i/43125355.jpg", 3, [])
+			new Book(
+				"Book1", 
+				"Placeholder description 1", 
+				"Placeholder link 1", 
+				"https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1543945452i/43125355.jpg", 
+				4, []),
+			new Book(
+				"Book2", 
+				"Placeholder description 2", 
+				"Placeholder link 2", 
+				"https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1543945452i/43125355.jpg", 
+				5, []),
+			new Book(
+				"Book3", 
+				"Placeholder description 3", 
+				"Placeholder link 3", 
+				"https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1543945452i/43125355.jpg", 
+				3, [])
 		];
 	}
 
