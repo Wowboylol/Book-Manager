5ede3162a0ad89e1e467fdca75bffc78fd44e40b
Lionel Song <99521745+Wowboylol@users.noreply.github.com>
2022-11-30 01:40:58 -0800

    Tags service refactoring

--- src/app/tags/tags.service.ts
+++ src/app/tags/tags.service.ts
@@ -14,25 +14,25 @@ export class TagService
 
     public addTag(tag:Tag)
     {
-        this.tags.unshift(tag);
-        this.tagChange.emit(this.tags.slice());
+        let nameFound = false;
+
+        for(let i=0; i<this.tags.length; i++)
+        {
+            if(tag.name == this.tags[i].name)
+                nameFound = true;
+        }
+        if(nameFound == false)
+        {
+            this.tags.unshift(tag);
+            this.tagChange.emit(this.tags.slice());
+        }
     }
 
     public addMultipleTags(tags:Tag[])
     {
         for(let i=0; i<tags.length; i++)
         {
-            const searchName = tags[i].name;
-            let nameFound = false;
-
-            for(let j=0; j<this.tags.length; j++)
-            {
-                if(searchName == this.tags[j].name)
-                    nameFound = true;
-            }
-
-            if(nameFound == false)
-                this.addTag(tags[i]);
+            this.addTag(tags[i]);
         }
     }
 
