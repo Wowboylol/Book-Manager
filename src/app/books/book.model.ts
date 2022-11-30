e8af5c82fadbe5bf645340905d0efa9ae992aec6
Lionel Song <99521745+Wowboylol@users.noreply.github.com>
2022-11-29 23:30:38 -0800

    Updated book model to include tag array

--- src/app/books/book.model.ts
+++ src/app/books/book.model.ts
@@ -1,3 +1,5 @@
+import { Tag } from "../shared/tag.model";
+
 export class Book
 {
     public name:string;
@@ -5,13 +7,15 @@ export class Book
     public link:string;
     public imagePath:string;
     public rating:number;
+    public tags:Tag[];
 
-    public constructor(name:string, description:string, link:string, imagePath:string, rating:number)
+    public constructor(name:string, description:string, link:string, imagePath:string, rating:number, tags:Tag[])
     {
         this.name = name;
         this.description = description;
         this.link = link;
         this.imagePath = imagePath;
         this.rating = rating;
+        this.tags = tags;
     }
 }
