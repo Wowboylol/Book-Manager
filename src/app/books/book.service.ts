8ed6d2ec4d767865b95908200087384578dd4833
Lionel Song <99521745+Wowboylol@users.noreply.github.com>
2022-11-29 22:09:28 -0800

    Added new methods in book service

--- src/app/books/book.service.ts
+++ src/app/books/book.service.ts
@@ -1,17 +1,20 @@
+import { EventEmitter } from "@angular/core";
 import { Book } from "./book.model";
 
 export class BookService
 {
-    private _books:Book[];
+    private books:Book[];
+    private selectedBook = new EventEmitter<Book>();
 
     public constructor() 
 	{ 
-		this._books = [
+		this.books = [
 			new Book("Book1", "Placeholder description 1", "Placeholder link 1", "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1543945452i/43125355.jpg", 4),
 			new Book("Book2", "Placeholder description 2", "Placeholder link 2", "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1543945452i/43125355.jpg", 5),
 			new Book("Book3", "Placeholder description 3", "Placeholder link 3", "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1543945452i/43125355.jpg", 3)
 		];
 	}
 
-    public getBooks():Book[] { return this._books.slice(); }
+    public getBooks():Book[] { return this.books.slice(); }
+    public getSelectedBook():EventEmitter<Book> { return this.selectedBook; }
 }
