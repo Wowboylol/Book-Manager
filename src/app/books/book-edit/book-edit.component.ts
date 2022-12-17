c07d81a450b5ad094a15c2e98c6c64ea1b2546d1
Lionel Song <99521745+Wowboylol@users.noreply.github.com>
2022-12-16 16:11:35 -0800

    Added dynamic tag changing

--- src/app/books/book-edit/book-edit.component.ts
+++ src/app/books/book-edit/book-edit.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, FormArray } from '@angular/forms';
 import { BookService } from '../book.service';
 
 @Component({
@@ -33,7 +33,18 @@ export class BookEditComponent implements OnInit
 		});
 	}
 
-	get bookForm():FormGroup { return this._bookForm; }
+	public get bookForm():FormGroup { return this._bookForm; }
+	public get controls() { return (<FormArray>this._bookForm.get('tags')).controls; }
+
+	onSubmit():void
+	{
+		console.log(this._bookForm);
+	}
+
+	onAddTag():void
+	{
+		
+	}
 
 	private initForm():void
 	{
@@ -42,6 +53,7 @@ export class BookEditComponent implements OnInit
 		let bookRating;
 		let bookLink = '';
 		let bookDescription = '';
+		let bookTags = new FormArray([]);
 
 		if(this._editMode)
 		{
@@ -51,6 +63,16 @@ export class BookEditComponent implements OnInit
 			bookRating = book.rating;
 			bookLink = book.link;
 			bookDescription = book.description;
+
+			if(book['tags'])
+			{
+				for(let tag of book.tags)
+				{
+					bookTags.push(
+						new FormGroup({'name': new FormControl(tag.name)})
+					);
+				}
+			}
 		}
 
 		this._bookForm = new FormGroup({
@@ -58,12 +80,8 @@ export class BookEditComponent implements OnInit
 			'imagePath': new FormControl(bookImagePath),
 			'rating': new FormControl(bookRating),
 			'link': new FormControl(bookLink),
-			'description': new FormControl(bookDescription)
+			'description': new FormControl(bookDescription),
+			'tags': bookTags
 		});
-	}
-
-	onSubmit():void
-	{
-		console.log(this._bookForm);
 	}
 }
