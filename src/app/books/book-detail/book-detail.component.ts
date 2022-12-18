844ea5879092ecdedbceb3cbb759804d8ba7ed6c
Lionel Song <99521745+Wowboylol@users.noreply.github.com>
2022-12-17 19:29:33 -0800

    Added tag search redirection from book edit

--- src/app/books/book-detail/book-detail.component.ts
+++ src/app/books/book-detail/book-detail.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
-import { TagService } from 'src/app/tags/tags.service';
 import { BookService } from '../book.service';
+import { SearchService } from 'src/app/search/search.service';
 import { Book } from '../book.model';
+import { SearchField } from 'src/app/shared/searchField.model';
+import { SearchFilter } from 'src/app/shared/searchFilter.model';
 
 @Component({
 	selector: 'app-book-detail',
@@ -15,14 +17,14 @@ export class BookDetailComponent implements OnInit
 	public selectedBook:Book;
 	private router:Router;
 	private route:ActivatedRoute;
-	private _tagService:TagService;
 	private _bookService:BookService;
+	private searchService:SearchService;
 	private bookID;
 
-	public constructor(tagService:TagService, bookService:BookService, router:Router, route:ActivatedRoute) 
+	public constructor(bookService:BookService, searchService:SearchService, router:Router, route:ActivatedRoute) 
 	{
-		this._tagService = tagService;
 		this._bookService = bookService;
+		this.searchService = searchService;
 		this.router = router;
 		this.route = route;
 	}
@@ -46,5 +48,11 @@ export class BookDetailComponent implements OnInit
 	{
 		this._bookService.deleteBook(this.bookID);
 		this.router.navigate(['/books']);
+	}
+
+	public redirectToTagSearch(tagName:string):void
+	{
+		this.router.navigate(['/search']);
+		setTimeout(() => { this.searchService.searchBooks(tagName, "2", "4"); }, 500);
 	}
 }
