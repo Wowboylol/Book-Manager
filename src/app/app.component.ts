07c09a4495f81d1aaa9418347314de2f65679ca3
Lionel Song <99521745+Wowboylol@users.noreply.github.com>
2022-12-20 18:03:24 -0800

    Print debugging/to-do notes to console

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
 	selector: 'app-root',
@@ -8,6 +9,8 @@ import { Component } from '@angular/core';
 
 export class AppComponent 
 {
-
-	public constructor() { }
+	public constructor() 
+	{ 
+		console.log(environment.notes);
+	}
 }
