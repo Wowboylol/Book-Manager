28da84c4edef963da422b07b64d2d2712983200e
Lionel Song <99521745+Wowboylol@users.noreply.github.com>
2023-02-12 17:32:06 -0800

    Add cooldown for saving/fetching books

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,15 +6,24 @@ import { DataStorageService } from '../shared/data-storage.service';
 
 @Component({
     selector: 'app-header',
-    templateUrl: './header.component.html'
+    templateUrl: './header.component.html',
+    styleUrls: ['./header.component.css']
 })
 
 export class HeaderComponent implements OnInit, OnDestroy
 {
+    // Attributes
     private userSubscription: Subscription;
 	public collapsed = true;
     public isAuthenticated = false;
 
+    // Alert attributes
+    public timer = null;
+    public success = false;
+    public error = false;
+    public message = "";
+
+    // Methods
     constructor(private dataStorage:DataStorageService, private authService:AuthService) { }
 
     ngOnInit() 
@@ -26,9 +35,39 @@ export class HeaderComponent implements OnInit, OnDestroy
 
     ngOnDestroy() { this.userSubscription.unsubscribe(); }
 
-    public onSaveData() { this.dataStorage.storeBooks(); }
+    public onSaveData() 
+    { 
+        if(this.onCooldown() == false)
+        {
+            this.success = true;
+            this.message = "Books saved successfully!";
+            this.timer = setTimeout(() => { this.timer = null; this.success = false; this.error = false; }, 5000);
+            this.dataStorage.storeBooks(); 
+        }
+    }
 
-    public onFetchData() { this.dataStorage.fetchBooks().subscribe(); }
+    public onFetchData() 
+    { 
+        if(this.onCooldown() == false)
+        {
+            this.success = true;
+            this.message = "Books fetched successfully!";
+            this.timer = setTimeout(() => { this.timer = null; this.success = false; this.error = false; }, 5000);
+            this.dataStorage.fetchBooks().subscribe(); 
+        }
+    }
+
+    private onCooldown(): boolean
+    {
+        if(this.timer) 
+        {
+            this.error = true;
+            this.success = false;
+            this.message = "Please wait 5 seconds before saving/fetching again!";
+            return true;
+        }
+        return false;
+    }
 
     public onLogout() { this.authService.logout(); }
 }
